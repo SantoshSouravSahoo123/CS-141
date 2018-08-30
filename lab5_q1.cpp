7f73a5b69b8f7728e8aff2eaf13d414702d77714
SantoshSouravSahoo123 <42238175+SantoshSouravSahoo123@users.noreply.github.com>
2018-08-30 14:46:39 +0000

    Add files via upload

---
+++ lab5_q1.cpp
@@ -0,0 +1,18 @@
+// first include the library
+#include<iostream>
+using namespace std;
+int main()
+ 	{
+// declaring the variables
+	int a,b;
+	cout<< " enter the two numbers " <<endl;
+	cin>>a>>b;
+	if(a>b){
+		cout<< " greatest number is " <<a<<endl;
+	}
+	else
+	{
+		cout<< " greatest number is " <<b<<endl;
+	}
+	return 0;
+	}
